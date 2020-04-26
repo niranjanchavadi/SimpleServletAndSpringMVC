19266a023482dd6ccaa151447b6100716c3f2f19
nnchavadi <nnchavadi2@gmail.com>
2020-04-26 22:44:17 +0530

    [niranjanchavadi]ADDED public enum conversio of celsius to fahrenheit celsuis taken as base unit

---
+++ quantitymeasurements/src/main/java/com/quantityComparator/model/TemperatureUnit.java
@@ -0,0 +1,26 @@
+package com.quantityComparator.model;
+
+
+public enum TemperatureUnit implements IUnit {
+    FAHRENHEIT(32.0), CELSIUS(1.0);
+
+    public final double measurementValue;
+
+    TemperatureUnit(Double measurementValue) {
+        this.measurementValue = measurementValue;
+    }
+
+    @Override
+    public Double conversion() {
+        if (this.equals(FAHRENHEIT))
+            return FAHRENHEIT.measurementValue;
+        return CELSIUS.measurementValue;
+    }
+
+    @Override
+    public String getUnit() {
+        return "celsius";
+    }
+
+
+}
