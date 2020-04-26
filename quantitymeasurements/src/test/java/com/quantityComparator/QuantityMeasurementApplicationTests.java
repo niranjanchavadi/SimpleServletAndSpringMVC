9ac3fa3e3e87f828ce5d0a4e9d8bd0c2359532f7
nnchavadi <nnchavadi2@gmail.com>
2020-04-26 23:50:40 +0530

    [

---
+++ quantitymeasurements/src/test/java/com/quantityComparator/QuantityMeasurementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantityComparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantityMeasurementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
