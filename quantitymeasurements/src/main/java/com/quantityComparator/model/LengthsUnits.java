9392e287c4f228efad2c484186510128fecbdce4
nnchavadi <nnchavadi2@gmail.com>
2020-04-26 22:42:40 +0530

    [niranjanchavadi]ADDED public enum has length conversion units taken inch as base unit to convert others like feet,yard....etc

---
+++ quantitymeasurements/src/main/java/com/quantityComparator/model/LengthsUnits.java
@@ -0,0 +1,23 @@
+package com.quantityComparator.model;
+
+public enum LengthsUnits implements IUnit {
+    FEET(12.0), INCH(1.0), YARD(36.0), CENTIMETER(0.4);
+
+
+    public final double measurementValue;
+
+    LengthsUnits(Double measurementValue) {
+        this.measurementValue = measurementValue;
+    }
+
+
+    @Override
+    public Double conversion() {
+        return this.measurementValue;
+    }
+
+    @Override
+    public String getUnit() {
+        return "INCH";
+    }
+}
