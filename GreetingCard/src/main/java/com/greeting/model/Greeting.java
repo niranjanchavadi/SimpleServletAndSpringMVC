7189b3521f440c58d0d613c6d05c4f4c4a8c8051
nnchavadi <nnchavadi2@gmail.com>
2020-04-26 20:01:58 +0530

    [niranjanchavadi]ADDED for pojo classes generated getter and setter to access that private  variables

---
+++ GreetingCard/src/main/java/com/greeting/model/Greeting.java
@@ -0,0 +1,52 @@
+package com.greeting.model;
+
+
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="GREETINGS")
+public class Greeting {
+    @Id
+    private  long id;
+
+    private String message;
+
+    public Greeting() {
+    }
+
+    public Greeting(long id, String message) {
+        this.id = id;
+        this.message = message;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "GreetingModel{" +
+                "id=" + id +
+                ", message='" + message + '\'' +
+                '}';
+    }
+
+
+}
---
+++ GreetingCard/src/main/java/com/greeting/model/User.java
@@ -0,0 +1,46 @@
+package com.greeting.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties
+public class User {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
